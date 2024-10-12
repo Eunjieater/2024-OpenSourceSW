2939270c51b8e8ef3504fe30139df91294fa23ed
Eunjieater <tlsdmswl999@gmail.com>
2024-10-12 15:07:37 +0900

    A와 B의 합

--- a_plus_b.py
+++ a_plus_b.py
@@ -1,13 +1,5 @@
-def week6(a, b, max):
-    i = 0
-    result = a*a*max+b
-    while i <= max:
-        week6(a, b, i)
-        i = i + 1
-    return (f'{a}x{a}x{max} + {b} = {result}')
+a = int(input('a : '))
+b = int(input('b : '))
 
-a = int(input('a: '))
-b = int(input('b: '))
-max = int(input('Max: '))
-week6(a, b, max)
+print(f'{a} + {b} = {a+b}')
 
